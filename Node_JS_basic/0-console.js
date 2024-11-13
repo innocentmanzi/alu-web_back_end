50d6bf8113af97661a921f46ecb3b6829046c726
innocentmanzi <i.manzi@alustudent.com>
2024-11-13 12:34:54 +0000

    NodeJS

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,15 +1,3 @@
-/**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
- */
-// Arrow function
-const displayMessage = (message) => console.log(message);
+const displayMessage = (str) => console.log(str);
 
-/**
- * Exports the displayMessage function.
- * @module displayMessage
- * @function
- */
-// Default module export.
 module.exports = displayMessage;
